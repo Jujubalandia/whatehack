5327f636330eff00c1ff1b9f25a0172cf3d84468
brunormoura <brunormoura@gmail.com>
2018-12-10 06:22:53 -0200

    Implemented  news api providres class and test class

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -5,20 +5,18 @@ import '../models/item_model.dart';
 final _root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
-
   Client client = Client();
-  
+
   fetchTopsIds() async {
-   final response =  await client.get('$_root/topstories.json');
-   final ids = json.decode(response.body);
-   return ids;
+    final response = await client.get('$_root/topstories.json');
+    final ids = json.decode(response.body);
+    return ids;
   }
 
   fetchItem(int id) async {
-     final response =  await client.get('$_root/item/$id.json');
-     final parsedJson = json.decode(response.body);
+    final response = await client.get('$_root/item/$id.json');
+    final parsedJson = json.decode(response.body);
 
-     return ItemModel.fromJson(parsedJson);
-
+    return ItemModel.fromJson(parsedJson);
   }
 }
