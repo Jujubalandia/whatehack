8b3e7a2172bce74589702b1b43088f50d72ec3f5
brunormoura <brunormoura@gmail.com>
2018-12-10 07:04:32 -0200

    Implementend news db provider init class constructor

---
+++ lib/src/resources/new_db_provider.dart
@@ -0,0 +1,38 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsdbProvider {
+  Database db;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABLE Items
+           (
+              id INTEGER PRIMARY KEY,
+              type TEXT,
+              time INTEGER,
+              text TEXT,
+              parent INTEGER,
+              kids BLOB,
+              dead INTEGER,
+              deleted INTEGER,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendents INTEGER
+           )
+        """);
+      },
+    );
+  }
+}
