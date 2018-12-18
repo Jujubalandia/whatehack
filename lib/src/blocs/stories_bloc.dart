2c15038fd19b3a86cf7a71572efff7f1e61babb3
brunormoura <brunormoura@gmail.com>
2018-12-18 07:54:33 -0200

    Implementing stories bloc and use stories provider in application

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,20 @@
-class StoriesBloc {
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
 
+class StoriesBloc {
+  final _repository = Repository();
+  final _topsIds = PublishSubject<List<int>>();
+
+  // Getter to streams
+  Observable<List<int>> get topIds => _topsIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopsIds();
+    _topsIds.sink.add(ids);
+  }
+
+  dispose() {
+    _topsIds.close();
+  }
 }
