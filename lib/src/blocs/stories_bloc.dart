a020463893a2dd90ed76f52c36f1bef0b1be4089
brunormoura <brunormoura@gmail.com>
2018-12-20 06:13:03 -0200

    Implemented cosntructor for and items observable

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -6,13 +6,19 @@ import '../resources/repository.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
-  final _items = BehaviorSubject<int>(); 
+  final _items = BehaviorSubject<int>();
+
+  Observable<Map<int, Future<ItemModel>>> items;
 
   // Getters to Streams
   Observable<List<int>> get topIds => _topIds.stream;
 
-  // Getters to Sinks 
-  Function(int)  get fetchItem => _items.sink.add;
+  // Getters to Sinks
+  Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc() {
+    items = _items.stream.transform(_itemTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -23,11 +29,11 @@ class StoriesBloc {
     return ScanStreamTransformer(
       (Map<int, Future<ItemModel>> cache, int id, _) {
         cache[id] = _repository.fetchItem(id);
-        return cache; 
+        return cache;
       },
       <int, Future<ItemModel>>{},
     );
-  } 
+  }
 
   dispose() {
     _topIds.close();
