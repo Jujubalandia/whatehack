2a734c54b78aef555ed7284a62f14fa21d52b158
brunormoura <brunormoura@gmail.com>
2018-12-20 06:04:42 -0200

    Added transform helper method and items Sink

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
@@ -5,16 +6,31 @@ import '../resources/repository.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>(); 
 
-  // Getter to streams
+  // Getters to Streams
   Observable<List<int>> get topIds => _topIds.stream;
+
+  // Getters to Sinks 
+  Function(int)  get fetchItem => _items.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  _itemTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache; 
+      },
+      <int, Future<ItemModel>>{},
+    );
+  } 
+
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
